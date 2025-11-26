28f4d72e0e14f82e9c4005e9ba90da9b11672bb3
Kedir Biruh <kedirbiruh1114@gmail.com>
2025-11-26 14:56:56 +0100

    Refactor app state management: replace AppStateAgent with AppStateController; implement add, toggle, and delete Todo functionalities; update pubspec for sqflite dependency

--- lib/providers/app_state_notifier.dart
+++ lib/providers/app_state_notifier.dart
@@ -1,82 +1,9 @@
 import 'package:flutter_riverpod/legacy.dart';
 import 'package:i12_into_012/models/app_state.dart';
-import 'package:i12_into_012/models/app_state_agent.dart';
-import 'package:i12_into_012/models/todo.dart';
-import 'package:i12_into_012/services/storage_service.dart';
 
 abstract class AppStateNotifier extends StateNotifier<AppState> {
-  AppStateAgent appStateAgent;
-  AppStateNotifier() 
-  : super(const AppState()) {
-    _init();
-  }
-
-  Future<void> _init() async {
-    final storage = StorageService();
-    final loaded = await storage.loadAppState();
-
-    if (loaded != null && loaded.todos.isNotEmpty) {
-      state = loaded;
-    } else {
-      state = state.copyWith(
-        todos: [
-          Todo.create('Einkaufen gehen'),
-          Todo.create('Flutter üben'),
-          Todo.create('Zimmer aufräumen'),
-        ],
-      );
-      await storage.saveAppState(state);
-    }
-  }
-
-  /*
-  void addTodo(String text) {
-    final newTodo = Todo.create(text);
-    state = state.copyWith(todos: [...state.todos, newTodo]);
-    _save();
-  }
-*/
-  void addTodo(String text) {
-    Todo newTodo = appStateAgent.addTodo(state, text);
-    state = appStateAgent.state;
-    sqliteAddTodo(todo);
-  }
-
-  AppState _addTodo(AppState state, String text) {
-    final newTodo = Todo.create(text);
-    return state.copyWith(todos: [...state.todos, newTodo]);
-  }
-
-  void toggleTodo(String id) {
-    state = state.copyWith(
-      todos: state.todos
-          .map((t) => t.id == id ? t.copyWith(isCompleted: !t.isCompleted) : t)
-          .toList(),
-    );
-    _save();
-  }
-
-  void deleteTodo(String id) {
-    state = state.copyWith(
-      todos: state.todos.where((t) => t.id != id).toList(),
-    );
-    _save();
-  }
-
-  void toggleDarkMode() {
-    state = state.copyWith(isDarkMode: !state.isDarkMode);
-    _save();
-  }
-
-  void toggleDeletionConfirmation() {
-    state = state.copyWith(
-      asksForDeletionConfirmation: !state.asksForDeletionConfirmation,
-    );
-    _save();
-  }
-
-  Future<void> _save() async {
-    final storage = StorageService();
-    await storage.saveAppState(state);
-  }
+  AppStateNotifier(AppState state) : super(state);
+  void addTodo(String text);
+  void toggleTodo(String id);
+  void deleteTodo(String id);
 }
